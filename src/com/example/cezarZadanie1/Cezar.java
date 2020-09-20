5b82d032c8046f1bf755df4ceee0309598b49bdc
Kotov Mikhail <kotov-mikhail@bk.ru>
2020-09-20 21:32:09 +0300

    Изменил имя переменной antkey на decryptKey

--- src/com/example/cezarZadanie1/Cezar.java
+++ src/com/example/cezarZadanie1/Cezar.java
@@ -4,10 +4,10 @@ import java.nio.file.ClosedWatchServiceException;
 
 public class Cezar {
     private int key;
-    private int antkey;
+    private int decryptKey;
 
-    public Cezar(int key, int antkey) {
-        this.antkey = antkey;
+    public Cezar(int key, int decryptKey) {
+        this.decryptKey = decryptKey;
         this.key = key;
     }
 
@@ -22,7 +22,7 @@ public class Cezar {
     public  char[] decrypt(String message) {
         char[] symbols = new char[message.length()];
         for (int i = 0; i < message.length(); i++)
-            symbols[i] = (char) ((int) (message.charAt(i) - antkey));
+            symbols[i] = (char) ((int) (message.charAt(i) - decryptKey));
         return symbols;
 
     }
